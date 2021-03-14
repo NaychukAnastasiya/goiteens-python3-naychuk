9db93db7ce0b7d966f1ab6b3191f06faa1870429
NaychukAnastasiya <nastyametelyk@gmail.com>
2021-03-14 13:26:18 +0200

    Added lesson3 tasks

---
+++ lesson3_work/Ex8.py
@@ -0,0 +1,4 @@
+pensil_box = ["pen","pensil","eraser","candy","gum"]
+print(pensil_box)
+pensil_box.append("betmen_statue")
+print(pensil_box)
---
+++ lesson3_work/Ex9.py
@@ -0,0 +1,8 @@
+pensil_box = ["pen","pensil","eraser","candy","gum"]
+pensil_box_friend =["paper", "pen"]
+if len(pensil_box) > len(pensil_box_friend):
+   print("в школяра більше предметів у пеналі")
+elif len(pensil_box_friend) > len(pensil_box):
+   print("у друга школяра більше предметів")
+else:
+    pass
