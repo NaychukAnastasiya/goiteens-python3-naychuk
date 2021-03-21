b0762fba0bf7bc38dbb9e512ce1155b837b4a524
NaychukAnastasiya <nastyametelyk@gmail.com>
2021-03-21 10:52:24 +0200

    Lesson_work_4_done

---
+++ lesson_4_work/Ex4.py
@@ -0,0 +1,9 @@
+names = ["Katya", "Max", "Oleksii", "Olesya", "Oleh", "Yaroslav", "Anastasiia"]
+age = [25, 54, 18, 23, 45, 21, 21]
+isPaid = [True, False, True, True, False, True, False]
+
+indMax = names.index("Max")
+names.pop(indMax)
+age.pop(indMax)
+isPaid.pop(indMax)
+print(names, age, isPaid)
---
+++ lesson_4_work/Ex5.py
@@ -0,0 +1,7 @@
+names = ["Katya", "Max", "Oleksii", "Olesya", "Oleh", "Yaroslav", "Anastasiia"]
+age = [25, 54, 18, 23, 45, 21, 21]
+isPaid = [True, False, True, True, False, True, False]
+names.insert(0,"Lev")
+age.insert(0, 36)
+isPaid.insert(0, True)
+print(names, age, isPaid)
