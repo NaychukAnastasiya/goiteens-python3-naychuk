7bead5e9022270a06a5cf2f84ebda1c70be0668d
NaychukAnastasiya <nastyametelyk@gmail.com>
2021-04-18 14:41:40 +0300

    Lesson8_work_done

---
+++ lesson8_work/Ex7.py
@@ -0,0 +1,7 @@
+# Написати функцію, яка перевіряє чи в списку є ім'я "Євген"
+def is_in_list(l, e):
+    if e in l:
+        return True
+    else:
+        return False
+print(is_in_list(['Ярослав', 'Богдан', 'Катя', 'Євген'], "Євген"))
